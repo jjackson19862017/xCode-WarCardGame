429ea1ae4a204820c088055f246f6946780de8bd
Stephen Jackson <jjackson19862017@gmail.com>
2020-04-16 00:51:15 +0100

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Stephen Jackson on 16/04/2020.
+//  Copyright © 2020 Stephen Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
